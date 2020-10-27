42c0fd18a4c668014658d05794673d5735b8407f
kibiti <beatuskibiti@gmail.com>
2020-10-27 13:01:30 +0300

    Initial configuration and template setting

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,213 @@
+ <!-- Sidebar Menu -->
+ <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+  
+      <li class="nav-item">
+        <a href="pages/calendar.html" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Dashboard
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/calendar.html" class="nav-link">
+          <i class="nav-icon far fa-calendar-alt"></i>
+          <p>
+            Calendar
+            <span class="badge badge-info right">2</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-user-friends"></i>
+          <p>
+            People
+            <i class="fas fa-angle-left right"></i>
+            
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="pages/layout/top-nav.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>View Persons</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/layout/top-nav.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>View Families</p>
+            </a>
+          </li>
+      
+       
+     
+        </ul>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-users"></i>
+          <p>
+            Groups
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>View Groups</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Group Members</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-calendar-alt"></i>
+          <p>
+            Events
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>ChartJS</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Inline</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-university"></i>
+          <p>
+            Deposits
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>ChartJS</p>
+            </a>
+          </li>
+   
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-money-check-alt"></i>
+          <p>
+            Fundraiser
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>ChartJS</p>
+            </a>
+          </li>
+   
+          </li>
+        </ul>
+      </li>
+   
+      <li class="nav-header">ADMINISTRATION</li>
+    
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-users-cog"></i>
+          <p>
+            User Management
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="pages/charts/chartjs.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Roles</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/flot.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Users</p>
+            </a>
+          </li>
+       
+        </ul>
+      </li>
+      <li class="nav-item has-treeview">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-table"></i>
+          <p>
+            Master Data
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>ChartJS</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Flot</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/inline.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Inline</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/flot.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Flot</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/inline.html" class="nav-link">
+              <i class="fas fa-angle-double-right"></i>
+              <p>Inline</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link">
+          <i class="nav-icon fas fa-cogs"></i>
+          <p>Settings</p>
+        </a>
+      </li>
+     
+    
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
